5d10d532a12ccf7499f733f3a33446386863ad07
cskusuma14 <48611465+cskusuma14@users.noreply.github.com>
2020-01-28 12:43:39 +0700

    Create exercise-3.js

---
+++ exercise-3.js
@@ -0,0 +1,35 @@
+//Exercise3
+var nama = "";
+var peran = "";
+
+if (nama == "")
+{
+    console.log("Nama harus diisi!");
+}
+else if (nama != "" && peran == "")
+{
+    console.log("Halo " + nama + ", Pilih peranmu untuk memulai game!");
+}
+else if (nama != "" && peran != "")
+{
+    if(peran == "Ksatria")
+    {
+        console.log("Selamat datang di Dunia Proxytia, " + nama);
+        console.log("Halo "+  peran +" "+ nama + ", kamu dapat menyerang dengan senjatamu!");
+    }
+    else if(peran == "Tabib")
+    {
+        console.log("Selamat datang di Dunia Proxytia, " + nama);
+        console.log("Halo "+  peran +" "+ nama + ", kamu akan membantu temanmu yang terluka.");
+    }
+    else if(peran == "Penyihir")
+    {
+        console.log("Selamat datang di Dunia Proxytia, " + nama);
+        console.log("Halo "+  peran +" "+ nama + ", ciptakan keajaiban yang membantu kemenanganmu!");
+    }
+    else
+    {
+        console.log("Peran tidak sesuai");
+    }
+}
+// else console.log("Error");
