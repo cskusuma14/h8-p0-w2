dc2afd4b91e3dee34e6c0addfd0aeab7ea34d1bb
cskusuma14 <48611465+cskusuma14@users.noreply.github.com>
2020-01-30 09:45:24 +0700

    Create exercise-7.js

---
+++ exercise-7.js
@@ -0,0 +1,39 @@
+1. Menyusun Barisan Bintang
+
+var rows = 5;
+
+for(var i =0; i<rows; i++)
+{
+    console.log("*");
+}
+
+-------------------------------------------------------
+
+2. Menyusun Barisan Bintang Dengan Nested Looping
+emp);
+
+var rows = 5;
+for(var i =0; i<rows; i++)
+{
+    var temp = "";
+    for(j=0; j<rows; j++)
+        {
+            temp += "*";         
+        }  
+        console.log(temp);
+}
+
+-------------------------------------------------------
+
+3. Menyusun Barisan Tangga Bintang Dengan Nested Looping
+
+var rows = 5;
+for(var i =0; i<rows; i++)
+{     
+  var temp = "";  
+    for(var j =0; j<=i; j++)
+    {
+      temp += "*";
+    }
+  console.log(temp);      
+}
